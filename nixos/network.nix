b7a90b879dc6c24e8023730e3aeda5792fc1b3e8
autumn <lirelum@disroot.org>
2025-08-04 23:45:25 -0400

    Fix avahi config

--- nixos/network.nix
+++ nixos/network.nix
@@ -5,6 +5,10 @@
     ipv4 = true;
     ipv6 = false;
     nssmdns4 = true;
-    nssmdns6 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      workstation = true;
+    };
   };
 }
