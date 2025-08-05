4e0a2a8c7b72205b77899dbf0223f8b471c2cd4a
autumn <lirelum@disroot.org>
2025-08-04 21:31:50 -0400

    Add mozc

--- nixos/i18n.nix
+++ nixos/i18n.nix
@@ -1,8 +1,13 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
   time.timeZone = lib.mkDefault "America/New_York";
 
   i18n = {
     defaultLocale = "ja_JP.UTF-8";
+    inputMethod = {
+      enable = true;
+      type = "ibus";
+      ibus.engines = with pkgs.ibus-engines; [mozc-ut];
+    };
   };
 }
