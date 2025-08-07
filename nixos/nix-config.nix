d80775d21a3240b63533e9db35477ab761d1950c
autumn <lirelum@disroot.org>
2025-08-06 21:22:31 -0400

    Add support for auto gc and store optimisation

--- nixos/nix-config.nix
+++ nixos/nix-config.nix
@@ -1,10 +1,18 @@
 { pkgs, ... }:
 {
   environment.systemPackages = [ pkgs.git ];
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix = {
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    optimise.automatic = true;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = "25.05";
 }
